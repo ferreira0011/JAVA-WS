ed5053359d6e8196d51e99ab678cd8fbd950ffc5
Victor Alves Ferreira <victorferreira0011@gmail.com>
2022-08-08 09:32:01 -0300

    Utilizando Final na classe e métodos

---
+++ 89_classes_e_metodos_final/src/entities/SavingsAccountPlus.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class SavingsAccountPlus extends SavingsAccount{
+	// Final não permite criar a classe com extends de SavingsAccount
+	
+	/* Final não permite criar método sobreposto da classe SavingsAccount
+	@Override
+	public void withdraw(double amount) {
+		balance -= amount + 2;
+	}
+	*/
+}
