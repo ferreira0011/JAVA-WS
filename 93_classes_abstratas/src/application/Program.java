63cb2d5552ff08c628fc69225747f2686683fd31
Victor Alves Ferreira <victorferreira0011@gmail.com>
2022-08-10 15:34:47 -0300

    93 classes abstratas

---
+++ 93_classes_abstratas/src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingsAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		/*
+		 * acc1 não pode instanciar a classe Account por ser abstrata Account acc1 = new
+		 * Account(1001, "Alex", 1000.0); Account acc2 = new SavingsAccount(1002,
+		 * "Maria", 1000.0, 0.01); Account acc3 = new BusinessAccount(1003, "Bob",
+		 * 1000.0, 500.0);
+		 */
+		
+		
+		// Adicionando valores as contas independente do tipo de conta por utilizar a classe abstrata.
+		List<Account> list = new ArrayList<>();
+
+		list.add(new SavingsAccount(1001, "Alex", 500.00, 0.01));
+		list.add(new BusinessAccount(1002, "Maria", 1000.0, 400.0));
+		list.add(new SavingsAccount(1004, "Bob", 300.0, 0.01));
+		list.add(new BusinessAccount(1005, "Anna", 500.0, 500.0));
+		
+		double sum = 0.0;
+		for(Account acc : list) {
+			sum += acc.getBalance();
+		}
+		
+		System.out.printf("Total balance: %.2f%n", sum);
+		
+		for(Account acc : list) {
+			acc.deposit(10.0);
+		}
+		
+		for(Account acc : list) {
+			System.out.printf("Updated balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+		}
+	}
+
+}
