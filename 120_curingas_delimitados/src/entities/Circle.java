2763dc91cb9b1f98a4e9493802ca03617278434f
Victor Alves Ferreira <84113257+ferreira0011@users.noreply.github.com>
2024-01-15 23:02:03 -0300

    curingas delimitados 1

---
+++ 120_curingas_delimitados/src/entities/Circle.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class Circle implements Shape {
+
+	private double radius;
+
+	public Circle(double radius) {
+		super();
+		this.radius = radius;
+	}
+
+	public double getradius() {
+		return radius;
+	}
+
+	public void setRaidus(double radius) {
+		this.radius = radius;
+	}
+
+	@Override
+	public double area() {
+		return Math.PI * (Math.pow(radius, 2));
+	}
+
+}
